d3bc4f0d9149ef810b0954b9b987554970602e8e
DanielNFeerica <daniel.neto@feerica.com>
2022-02-01 10:03:29 +0000

    Array de filmes nas views

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilmes } from '../models/IFilmes';
 
 
 
@@ -10,6 +11,34 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulos = 'Videos App';
+  listaVideos: IFilmes[] = [
+    {
+      nome: 'Homem-Aranha',
+      lancamento: '17-12-2021 (US)',
+      duracao: '2h 28m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg',
+      generos: ['Ação', 'Aventura', 'Ficção científica']
+    },
+    {
+      nome: 'Chernobyl: Abyss (2021)',
+      lancamento: '15-04-2021 (RU)',
+      duracao: '2h 16m',
+      classificacao: 63,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bnB2EkaY6HEdzwVkkH7dBHy6HmZ.jpg',
+      generos: ['Drama', 'História', 'Aventura']
+    },
+    {
+      nome: 'Hotel Transylvania 4 (2022)',
+      lancamento: '13-01-2022 (US',
+      duracao: '1h 28m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9PbtCo5IIkd26WPQfZUpPyn6fTz.jpg',
+      generos: ['Animação', 'Família', 'Fantasia', 'Comédia', 'Aventura']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
